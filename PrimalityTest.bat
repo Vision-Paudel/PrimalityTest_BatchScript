94718a0cd96eb6e5f7dadedba99fbe5a9b834dd4
Vision-Paudel <32627507+Vision-Paudel@users.noreply.github.com>
2019-09-08 00:08:55 -0400

    Update PrimalityTest.bat

--- PrimalityTest.bat
+++ PrimalityTest.bat
@@ -1,41 +1,49 @@
 @echo off
-REM Primality Test by Vision Paudel
-echo Primality Test by Vision Paudel
-
 Color 02
+echo * ----------------------------------------------------------- *
+echo * Primality Test by Vision Paudel                             *
+echo * https://github.com/Vision-Paudel/PrimalityTest_BatchScript  *
+echo * ----------------------------------------------------------  * 
 
 :NEWNUMBER
 set /p number="Enter an integer number: "
 echo %number%|findstr /r /c:"^[0-9][0-9]*"$ >nul
 if errorlevel 1 (GOTO NotValidNumber)
 
+REM First Five Cases
 if %number%==1 GOTO NOTPRIME
 if %number% gtr 1 if %number% leq 3 GOTO PRIME
 if %number%==4 GOTO NOTPRIME
 if %number%==5 GOTO PRIME
 
+REM Check If Divisible by 2
 set divisor=2
 echo Divide by %divisor%
 set /a mod= %number% %% %divisor%
 if %mod%==0 GOTO NOTPRIME 
  
+REM Set New Max to check up to
 set /a newMax = %number% / %divisor% 
+
+REM | * ------------------------------------------------------------------------- * |
+REM | * Loop to check rest of the numbers starting at 3                           * |
+REM | * and going up by 2 until divisor is approximately the square of the number * |
+REM | * ------------------------------------------------------------------------- * |
+
 set divisor=3
 
-:LOOP2
+:LOOP
 
 echo Divide by %divisor%
 set /a mod= %number% %% %divisor%
+
 if %mod%==0 GOTO NOTPRIME 
-if %divisor% geq %newMax% GOTO NOTPRIME
-echo newMax is %newMax%
+if %divisor% geq %newMax% GOTO PRIME
+
+set /a newMax = %number% / %divisor% 
 set /a divisor = %divisor% + 2
 
-GOTO LOOP2
-
-Color
-pause
-exit
+GOTO LOOP
 
 :PRIME
 echo %number% is a prime number!
